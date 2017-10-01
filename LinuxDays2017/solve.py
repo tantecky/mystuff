ca6570459b3d45afd8bbab958841629dabdbb7d2
Tomas Antecky <tomas@antecky.cz>
2017-10-01 16:13:47 +0200

    LinuxDays2017: Add more comments

--- LinuxDays2017/solve.py
+++ LinuxDays2017/solve.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import r2pipe
 
+#  open the binary with attached debugger
 r2 = r2pipe.open('./runme', ['-d'])
 
 while 'invalid' not in r2.cmd('s'):
     #  do one step + seek to rip register
     r2.cmd('ds;sr rip')
 
+    # dissamble one instruction
     json = r2.cmdj('pdj 1')
 
     if not json:
@@ -15,9 +17,13 @@ while 'invalid' not in r2.cmd('s'):
     json = json[0]
     opcode = json['opcode']
 
+    #  identify an instruction with a password
     if 'cmp byte [rcx]' in opcode:
+        #  extract next character of a password
         char = str(hex(json['ptr']))
+        # write the character to stack
         r2.cmd('wx {} @ rcx'.format(char))
+        #  print the character
         print(chr(int(char, 16)), end='', flush=True)
 
 print()
